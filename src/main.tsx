5d731d6480b288753055b0681f27563047343075
vitoria74 <maria_vitoria74@hotmail.com>
2024-08-05 14:51:14 -0300

    padrão corrigido

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import React from 'react' //global
 import ReactDOM from 'react-dom/client' //liga react a web
-import App from './App.tsx'
-import './index.css'
+import { App } from './app'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
